698ee8793ac09306f5d6885de3f1881478aa73ca
Qosdil A. <qosdil@yahoo.com>
2026-03-24 20:40:00 +0800

    fix(be-post): "users" table shouldn't be a part of Post service

--- backend/post/schema.sql
+++ backend/post/schema.sql
@@ -1,11 +1,3 @@
-CREATE TABLE public.users (
-    id serial NOT NULL,
-    created_at timestamp without time zone DEFAULT now() NOT NULL,
-    updated_at timestamp without time zone DEFAULT now() NOT NULL
-);
-
-ALTER TABLE public.users ADD CONSTRAINT users_pkey PRIMARY KEY (id);
-
 CREATE TABLE public.posts (
     id serial NOT NULL,
     public_id text unique NOT NULL,
@@ -16,8 +8,6 @@ CREATE TABLE public.posts (
 );
 
 ALTER TABLE public.posts ADD CONSTRAINT posts_pkey PRIMARY KEY (id);
-ALTER TABLE public.posts
-    ADD CONSTRAINT posts_relation_user_id FOREIGN KEY (user_id) REFERENCES public.users(id) ON UPDATE CASCADE ON DELETE CASCADE;
 
 CREATE TABLE public.post_likes (
     id serial NOT NULL,
@@ -30,5 +20,3 @@ ALTER TABLE public.post_likes ADD CONSTRAINT post_likes_pkey PRIMARY KEY (id);
 CREATE UNIQUE INDEX post_likes_unique ON public.post_likes USING btree (post_id, user_id);
 ALTER TABLE public.post_likes
     ADD CONSTRAINT post_likes_post_id FOREIGN KEY (post_id) REFERENCES public.posts(id) ON UPDATE CASCADE ON DELETE CASCADE;
-ALTER TABLE public.post_likes
-    ADD CONSTRAINT post_likes_user_id FOREIGN KEY (user_id) REFERENCES public.users(id) ON UPDATE CASCADE ON DELETE CASCADE;
