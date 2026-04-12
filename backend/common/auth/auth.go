19bbeb86750ad78e5b4976e4a9da18911ad0601b
Qosdil A. <qosdil@yahoo.com>
2026-04-12 21:55:36 +0800

    feat(be-common-auth): create a password hash generator

--- backend/common/auth/auth.go
+++ backend/common/auth/auth.go
@@ -1,6 +1,12 @@
 package auth
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"golang.org/x/crypto/bcrypt"
+)
 
 type auth struct{}
 
@@ -14,4 +20,25 @@ func (a *auth) CompareHashAndPassword(hash, password string) error {
 	}
 
 	return nil
+}
+
+// GeneratePasswordHash generates a bcrypt hash of the given password using the specified cost.
+func (a *auth) GeneratePasswordHash(password string) (string, error) {
+	cost := bcrypt.DefaultCost
+	envCost := os.Getenv("BCRYPT_GENERATE_FROM_PASSWORD_COST")
+	if len(envCost) > 0 {
+		costInt, err := strconv.Atoi(envCost)
+		if err != nil {
+			return "", fmt.Errorf("invalid cost value: %v", err)
+		}
+
+		cost = costInt
+	}
+
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), cost)
+	if err != nil {
+		return "", fmt.Errorf("failed to hash password: %v", err)
+	}
+
+	return string(hash), nil
 }
